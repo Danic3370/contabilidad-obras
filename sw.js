b9fe4ad6feccd76ffca4d6fe04c9e4abdf1f6db1
Danic3370 <115851114+Danic3370@users.noreply.github.com>
2026-08-08 15:53:16 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,10 +1,19 @@
 // Service Worker - Contabilidad Obras
-const CACHE = 'obras-v1';
-const ASSETS = ['/', '/index.html', '/manifest.json'];
+const CACHE = 'obras-v2';
+const ASSETS = [
+  '/', '/index.html', '/manifest.json',
+  '/favicon.ico', '/favicon-16.png', '/favicon-32.png', '/favicon-48.png',
+  '/icon-192.png', '/icon-512.png',
+  '/icon-maskable-192.png', '/icon-maskable-512.png',
+  '/apple-touch-icon.png'
+];
 
 self.addEventListener('install', e => {
   e.waitUntil(
-    caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())
+    caches.open(CACHE)
+      // Se cachea cada archivo por separado: si uno falta, no se cae la instalación
+      .then(c => Promise.all(ASSETS.map(a => c.add(a).catch(() => {}))))
+      .then(() => self.skipWaiting())
   );
 });
 
